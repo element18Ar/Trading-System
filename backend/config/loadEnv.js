f3fe62e99a9c74b07741b1109fb26bab0bae9eb4
element18Ar <aralbarida7@gmail.com>
2025-11-19 15:07:17 +0800

    feat: update environment loading to use dotenv-flow and add auth-service configuration files

--- backend/config/loadEnv.js
+++ backend/config/loadEnv.js
@@ -1,13 +1,14 @@
+import DotenvFlow from "dotenv-flow";
 import path from "path";
 import { fileURLToPath } from "url";
 
-export function loadEnv(moduleUrl, dotenvLib) {
+export function loadEnv(moduleUrl) {
   const __filename = fileURLToPath(moduleUrl);
   const __dirname = path.dirname(__filename);
 
   // Load global .env
-  dotenvLib.config({ path: path.resolve(__dirname, "../") });
+  DotenvFlow.config({ path: path.resolve(__dirname, "../") });
 
   // Load service-specific .env
-  dotenvLib.config({ path: path.resolve(__dirname, ".") });
+  DotenvFlow.config({ path: path.resolve(__dirname, ".") });
 }
