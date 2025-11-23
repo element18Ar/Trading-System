b20c0b62dfccc6f08fb20c7101251e7a6493ee8e
element18Ar <aralbarida7@gmail.com>
2025-11-23 16:24:41 +0800

    Add react-router-dom dependency and refactor App component structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Homepage from './pages/homepage.jsx'
-import Login from './pages/loginpage.jsx'
-import Register from './pages/registerpage.jsx'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Homepage from "./pages/homepage.jsx";
+import Login from "./pages/loginpage.jsx";
+import Register from "./pages/registerpage.jsx";
 
 function App() {
-
   return (
-    <Router>  
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
